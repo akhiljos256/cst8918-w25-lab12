1a8f01a1c9957d5c6ada4de315ac8288291b6ad5
thoufeekx <thoufeekbaber1@gmail.com>
2025-03-28 23:29:01 -0400

    Add Terraform backend and app infrastructure configurations

---
+++ infra/tf-backend/main.tf
@@ -0,0 +1,55 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+  required_version = ">= 1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "040982662-githubactions-rg"
+  location = "canadacentral"
+}
+
+# Storage Account
+resource "azurerm_storage_account" "sa" {
+  name                     = "040982662githubactions"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  min_tls_version = "TLS1_2"
+}
+
+# Storage Container
+resource "azurerm_storage_container" "container" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.sa.name
+  container_access_type = "private"
+}
+
+# Outputs
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.sa.name
+}
+
+output "container_name" {
+  value = azurerm_storage_container.container.name
+}
+
+output "arm_access_key" {
+  value     = azurerm_storage_account.sa.primary_access_key
+  sensitive = true
+}
